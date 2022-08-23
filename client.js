be9e7f61e168dfff07fae08b71c12b9bec83e56c
petestapp <63821853+petestapp@users.noreply.github.com>
2022-08-23 15:04:46 -0500

    single digit firstNumber set

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
-console.log("javascripting")
+console.log(`javascripting`);
+
+let firstNumber;
+let secondNumber;
+
+function clickButton() {
+    console.log(button.value + ` clicked`);
+}
+
+function stageFirstNumber(el) {
+    console.log(`button pressed: ` + el);
+    firstNumber = el;
+    console.log(`firstNumber: ` + firstNumber);
+}
+
+function stageOperator(el) {
+    console.log(`operator pressed: ` + el);
+}
