877e137df9aa56ee9ea6200604f800ff2908fb35
petestapp <63821853+petestapp@users.noreply.github.com>
2022-08-23 15:46:11 -0500

    single digit operations working in console

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ let firstNumber;
 let firstNumberPicked = false;
 let secondNumber;
 let operator;
+let answer;
 
 function clickButton() {
     console.log(`firstNumberPicked: ` + firstNumberPicked);
@@ -12,11 +13,12 @@ function clickButton() {
 function stageNumbers(el) {
     if (firstNumberPicked == false) {
         console.log(`button pressed: ` + el);
-        firstNumber = el;
+        firstNumber = Number(el);
         console.log(`firstNumber: ` + firstNumber);
         firstNumberPicked = true;
-    } else {
-        secondNumber = el;
+    }
+    else {
+        secondNumber = Number(el);
         console.log(`secondNumber: ` + secondNumber);
     }
 }
@@ -24,8 +26,31 @@ function stageNumbers(el) {
 function stageOperator(el) {
     if (firstNumberPicked == false) {
         console.log(`pick a number first`);
-    } else {
+    }
+    else {
         operator = el;
         console.log(`operator: ` + operator);
+    }
+}
+
+function completeOperation() {
+    if (firstNumberPicked == 0 || operator == null || secondNumber == null) {
+        console.log('error: add more info');
+    }
+    else if (operator == "divide") {
+        answer = firstNumber / secondNumber;
+        console.log(`answer: ` + answer);
+    }
+    else if (operator == "multiply") {
+        answer = firstNumber * secondNumber;
+        console.log(`answer: ` + answer);
+    }
+    else if (operator == "subtract") {
+        answer = firstNumber - secondNumber;
+        console.log(`answer: ` + answer);
+    }
+    else {
+        answer = firstNumber + secondNumber;
+        console.log(`answer: ` + answer);
     }
 }
