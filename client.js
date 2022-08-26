2e9e2369eccb6d13e81ed676536ee84f2aed7707
petestapp <63821853+petestapp@users.noreply.github.com>
2022-08-26 15:57:17 -0500

    added comments

--- client.js
+++ client.js
@@ -32,36 +32,43 @@ function clearData() {
     secondNumber = false;
 }
 
-let multipleDigits = [];
+// let multipleDigits = [];
 
-function multipleDigitTest(el) {
-    let num = multipleDigits.push(el);
-    console.log(`multipleDigits: ` + multipleDigits.join(''));
-    $(`#multipleDigits`).empty().append(Number(multipleDigits.join('')));
-    if (firstDecimalPointPresent == false) {
-        console.log(`multipleDigits: ` + multipleDigits);
-        $(`#multipleDigits`).empty().append(Number(multipleDigits.join('')));
-    }
-    else {
-        console.log(`can't do that`)
-    }
-}
+// function multipleDigitTest(el) {
+//     let num = multipleDigits.push(el);
+//     console.log(`multipleDigits: ` + multipleDigits.join(''));
+//     $(`#multipleDigits`).empty().append(Number(multipleDigits.join('')));
+//     if (firstDecimalPointPresent == false) {
+//         console.log(`multipleDigits: ` + multipleDigits);
+//         $(`#multipleDigits`).empty().append(Number(multipleDigits.join('')));
+//     }
+//     else {
+//         console.log(`can't do that`)
+//     }
+// }
 
 function stageNumbers(el) {
     if (firstNumberPicked == false) {
+        // if firstNumber has not been set, go to next if statement
         if (firstDecimalPointPresent == false) {
+            // if there is no decimal point specified yet, go to next if statement
             if (el == `.` && firstNumberArray.length == 0) {
+                // if '.' is selected before any digits have been selected, ERROR
                 console.log(`select a digit first`);
             }
             else if (el == `.`) {
+                // if '.' is selected after any digits, add the decimal point to the array and set firstDecimalPointPresent to true to prevent another decimal point from being selected
                 firstDecimalPointPresent = true;
                 firstNumberArray.push(el);
             }
             else {
+                // if your just pressing digits, there is no problem adding them to the array
                 firstNumberArray.push(el);
             }
         }
         else {
+            // if a decimal point has been selected, you cannot do another one
+            // (try to figure out a way to consolidate this with the above logic that's the same)
             if (el == `.`) {
                 console.log(`can't have more than one decimal point`);
             }
@@ -72,14 +79,9 @@ function stageNumbers(el) {
         $(`#screen`).empty().append(firstNumberArray);
         console.log(firstNumberArray);
     }
-    // else if (operatorPicked == false) {
-    //     console.log(`operator has not been picked`);
-    //     firstNumber = Number(el);
-    //     console.log(`firstNumber: ` + firstNumber);
-    //     $(`#screen`).empty().append(firstNumber);
-    // }
     else {
-        // if (firstNumberPicked == false) {
+        // this is the same logic as the first number in the function
+        // (another place to consolidate?)
         if (secondDecimalPointPresent == false) {
             if (el == `.` && secondNumberArray.length == 0) {
                 console.log(`select a digit first`);
@@ -102,18 +104,16 @@ function stageNumbers(el) {
         }
         $(`#screen`).empty().append(secondNumberArray);
         console.log(secondNumberArray);
-        // }
-        // secondNumber = Number(el);
-        // console.log(`secondNumber: ` + secondNumber);
-        // $(`#screen`).append(secondNumber);
     }
 }
 
 function stageOperator(el) {
     if (firstNumberArray.length == 0) {
+        // you cannot set the operator before selecting first number
         console.log(`pick a number first`);
     }
     else {
+        // if the first number has been selected this transforms it from an array to a number and saves it as firstNumber and sets the operator
         firstNumber = Number(firstNumberArray.join(''));
         operator = el;
         operatorPicked = true;
@@ -125,6 +125,7 @@ function stageOperator(el) {
 
 function completeOperation() {
     secondNumber = Number(secondNumberArray.join(''));
+    // once the equals sign is pressed this saves the secondNumber as a number rather than an array and the below lines complete the operation depending on the operator
     console.log(`secondNumber: `, secondNumber);
     if (firstNumberPicked == 0 || operator == null || secondNumber == null) {
         console.log('error: add more info');
